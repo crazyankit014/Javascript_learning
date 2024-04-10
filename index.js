6dd3322e1ac4c6037ee20d9c707419f507383119
Ankit <krankit014.ankit@gmail.com>
2024-04-10 18:30:14 +0530

    const myName ='Ankit';
    let myMail ="ankit@google.com";
    
    var myPassword='123456';

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-let age=20;
-if(age > 18)
-{
-    console.log("vote");
-}
-else{
-    console.log("not vote");
-}
+// let age=20;
+// if(age > 18)
+// {
+//     console.log("vote");
+// }
+// else{
+//     console.log("not vote");
+// }
+
+
+//   variables
+
+const myName ='Ankit';
+
+let myMail ="ankit@google.com";
+
+var myPassword='123456';
+
+
