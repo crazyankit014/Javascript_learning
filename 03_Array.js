db104dea1eea5d277bd461752c74404008a8a4d8
Ankit <krankit014.ankit@gmail.com>
2024-04-11 18:31:38 +0530

    changes

--- 03_Array.js
+++ 03_Array.js
@@ -1,3 +1,19 @@
 
 const myArr =[0,1,2,3,4,5]
-console.table(myArr);
+//console.table(myArr);
+
+// Array Method
+
+// 1) push (add values in array)
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+
+// myArr.unshift(9) // add value at the beginning of an array
+//myArr.shift() // remove the first value from the array 
+
+//console.log(myArr); // [0, 1, 2, 3, 4,  5, 6]
+
+console.log(myArr.indexOf(3)); // return the index position of a specific element
+console.log(myArr.includes(9));
