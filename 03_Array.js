55551b287e6298311d51fe8298ff2d66e282b7b2
Ankit <krankit014.ankit@gmail.com>
2024-04-12 01:04:18 +0530

    changes

--- 03_Array.js
+++ 03_Array.js
@@ -1,4 +1,3 @@
-const { Logger } = require("mongodb");
 
 const myArr =[0,1,2,3,4,5]
 //console.table(myArr);
@@ -24,21 +23,6 @@ const myArr =[0,1,2,3,4,5]
 // console.log( typeof newArr); // typeof string
 
 
-// slice , splice
-
-
-// console.log('A' , myArr);
-
-<<<<<<< HEAD
-const myn1 = myArr.slice(1,3);
-// console.log(myn1);
-// console.log('B', myArr);
-
-
-const myn2 = myArr.splice(1,3);
-// console.log('c',myArr);
-// console.log(myn2);
-
 
 const marvel_heros =["thor", 'Ironman','spiderman']
 const dc=['superman','flash','batman']
@@ -63,13 +47,30 @@ const allHerosSpread = [...marvel_heros, ...dc]
 // console.log(allHerosSpread)
 
 
-=======
+
 const myn1 = myArr.slice(1,3); // it does not maupilate the original array and it excludes last value
-console.log(myn1);
-console.log('B', myArr);
+// console.log(myn1);
+// console.log('B', myArr);
 
 
 const myn2 = myArr.splice(1,3); // it manipulate the original array and it includes the last value
-console.log('c',myArr);
-console.log(myn2);
->>>>>>> 367046481ffd56e762913235cf256cf90f0c727c
+// console.log('c',myArr);
+// console.log(myn2);
+
+
+// flat used to concat all array or multiple array in a single array
+const another_array=[1,2,3,[4,5,6],7,[6,7,[8,9]]];
+
+const real_another_array=another_array.flat(Infinity)
+
+// console.log(real_another_array);
+
+console.log(Array.isArray('Ankit')); // isArray is used to check  whether this argument is an array or not? 
+
+console.log(Array.from('Ankit'));  // from is used to convert a  string into an array of that string
+
+let score1=100;
+let score2=200;
+let score3=300;
+
+console.log(Array.of(score1,score2,score3));   // Array.of() is used to convert multiple values into array
